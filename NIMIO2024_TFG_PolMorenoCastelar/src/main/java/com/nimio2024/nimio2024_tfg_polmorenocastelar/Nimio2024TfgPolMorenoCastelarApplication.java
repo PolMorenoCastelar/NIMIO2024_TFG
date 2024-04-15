6ef964f338e309f67ed14c53ae90a73319854ea2
Pol Moreno Castelar <97445022+Pomocamal@users.noreply.github.com>
2024-04-15 15:42:11 +0200

    NPP-1

---
+++ NIMIO2024_TFG_PolMorenoCastelar/src/main/java/com/nimio2024/nimio2024_tfg_polmorenocastelar/Nimio2024TfgPolMorenoCastelarApplication.java
@@ -0,0 +1,13 @@
+package com.nimio2024.nimio2024_tfg_polmorenocastelar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nimio2024TfgPolMorenoCastelarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nimio2024TfgPolMorenoCastelarApplication.class, args);
+    }
+
+}
---
+++ NIMIO2024_TFG_PolMorenoCastelar/src/test/java/com/nimio2024/nimio2024_tfg_polmorenocastelar/Nimio2024TfgPolMorenoCastelarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nimio2024.nimio2024_tfg_polmorenocastelar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nimio2024TfgPolMorenoCastelarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
